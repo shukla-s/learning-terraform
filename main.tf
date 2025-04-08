402dbe8dda2967b3c6f82c63a38da98bde9a5956
shukla-s <me.sandeepshukla@gmail.com>
2025-04-08 22:34:39 +0100

    03_04

--- main.tf
+++ main.tf
@@ -19,9 +19,8 @@ data "aws_vpc" "default" {
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.blog.id]
 
   tags = {
@@ -31,8 +30,9 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name = "blog"
-  description = "Allow http and https in. Allow everything out"
-
+  tags = {
+    Terraform = "true"
+  }
   vpc_id = data.aws_vpc.default.id
 }
 
@@ -42,9 +42,9 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-
   security_group_id = aws_security_group.blog.id
 }
+
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
@@ -52,9 +52,9 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
-
   security_group_id = aws_security_group.blog.id
 }
+
 
 resource "aws_security_group_rule" "blog_everything_out" {
   type        = "egress"
@@ -62,6 +62,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-
   security_group_id = aws_security_group.blog.id
 }
